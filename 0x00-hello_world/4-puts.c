891bd2691f8f2b8544bded8005af369827d2c811
Timmy51 <adedejitimi5@gmail.com>
2023-03-17 01:09:55 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
- *main - This is a program that uses the puts function 
- *returns 0
+ *main - This is a program that uses the puts function
+ *return: 0
  */
 
 int main(void)

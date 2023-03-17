f898e2ac10c230fda0efe22b466b182db282d99b
Timmy51 <adedejitimi5@gmail.com>
2023-03-17 00:51:31 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,5 +2,7 @@
 /*This is a program that uses the puts function and returns 0*/
 
 int main(void)
-{puts ("\"Programming is like building a multilingual puzzle");
-return(0);}	
+{
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
+}	

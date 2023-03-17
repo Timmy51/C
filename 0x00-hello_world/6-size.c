eecc5ad6095c62d8b05a750174459b52f83ddde4
Timmy51 <adedejitimi5@gmail.com>
2023-03-17 03:47:07 -0700

    Size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,11 @@
  */
 int main(void)
 {
-char c
-int i
-long int l
-long long int ll
-float f
+char c;
+int i;
+long int l;
+long long int ll;
+float f;
 
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(i));

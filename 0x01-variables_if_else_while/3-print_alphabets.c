7b0d91d64858d8e0c399a1979bcc7c86ab027a6d
Timmy51 <adedejitimi5@gmail.com>
2023-03-20 04:02:42 -0700

    I sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include<stdio>
+/**
+ * main - a program that prints the alphabet in lower and uppercase
+ * Return: Always 0 Success
+ */
+
+int main(void)
+{
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVXYZ";
+int i;
+for (i = 0; i < 52; i++)
+{
+putchar(alp[i]);
+}
+putchar('\n')
+}

93fd2a71c009e8b38b81061f77567080a3b6c7c3
Timmy51 <adedejitimi5@gmail.com>
2023-03-20 07:07:35 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,9 +6,8 @@
 
 int main(void)
 {
-char n(9) = 0,1,2,3,4,5,6,7,8,9;
-int i
-for (i = 0; i < 10; i++)
+char i;
+for (i = 0; i <= '9'; i++)
 /*if (i != 'q' && i != 'e')*/
 putchar(i);
 putchar('\n');

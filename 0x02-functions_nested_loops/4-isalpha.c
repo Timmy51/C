57499887655ee414d0cabe6beeb4564294fbd371
Timmy51 <adedejitimi5@gmail.com>
2023-03-21 05:21:05 -0700

    isALPHA

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isalpha - checks for lowercase
+ * Return: Result
+ * parameter: integer
+ * @c: parameter
+ */
+int _isalpha(int c)
+{
+int result;
+if (c >= 97 && c <= 122) || (c >= 65 && c <= 90)
+result = 1;
+else
+result = 0;
+return (result);
+}

a656b2a575d3f5e5332cde86c2bfef016ccbea38
Timmy51 <adedejitimi5@gmail.com>
2023-03-21 07:17:41 -0700

    signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,17 +9,17 @@ int print_sign(int n)
 int result;
 if (n > 0)
 {
-_putchar(+);
+_putchar(43);
 result = 1;
 }
 else
 if (n < 0)
 {
-_putchar(-);
+_putchar(-45);
 result = -1;
 }
 else if (n == 0)
-_putchar(0);
+_putchar(48);
 result = 0;
 return (result);
 }

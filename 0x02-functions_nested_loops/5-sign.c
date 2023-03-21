8d28279b3f96a89d41391c390487a228cb0ad9ab
Timmy51 <adedejitimi5@gmail.com>
2023-03-21 06:06:19 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,20 +13,12 @@ _putchar('+');
 result = 1;
 }
 else
-{
-if (n = 0);
-{
+if (n = 0)
 _putchar('0');
 result = 0;
-}
 else
-{
 if (n < 0)
-{
 _putchar('-');
 result = -1;
-}
-}
-}
 return (result);
 }

8095f671e5ba3ba253ac70378d9413af5f9dadca
Timmy51 <adedejitimi5@gmail.com>
2023-03-22 01:11:44 -0700

    Times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * times_table - prints the times table from 0 to 9
+ * Return: does not apply
+ */
+void times_table(void)
+{
+int a, b;
+while ((a = 0 && a <= 9; a++) && (b = 0 && b <= 9; b++))
+{
+_putchar(a * b, );
+}
+}

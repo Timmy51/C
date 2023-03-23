4815d8359ed4d4d8479aa7aabfbf0611d3c6d3e8
Timmy51 <adedejitimi5@gmail.com>
2023-03-23 05:27:57 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * print_most_numbers - prints 0 to 9 without 2 and 4
- * @c: variable for the character to be checked
+ * Return: void
  */
 
 void print_most_numbers(void)
 {
 int c;
-for (c = 48; c < 58; c++)
-if (c != 49 && c != 51)
-_putchar(c + 48);
+for (c = '0'; c <= 9; c++)
+if (c != '2' && c != '4')
+_putchar(c);
 _putchar('\n');
 }

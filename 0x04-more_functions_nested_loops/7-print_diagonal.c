0acf5aa6ab9c5bcbd58af79af78b1e2b00addd8a
Timmy51 <adedejitimi5@gmail.com>
2023-03-24 03:52:10 -0700

    print square

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,12 +6,24 @@
  */
 void print_diagonal(int n)
 {
-n = 0;
-while (n > 0)
+if (n <= 0)
 {
-_putchar(92);
 _putchar('\n');
-n++;
+} else
+{
+int i, j;
+
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < n; j++)
+{
+if (j == i)
+_putchar('\\');
+else if (j < i)
+_putchar(' ');
 }
 _putchar('\n');
 }
+}
+}
+
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,11 +11,16 @@ if (size <= 0)
 _putchar('\n');
 }
 else
-{int i;
-for (i = 1; i <= size; i++)
+{
+int i, j;
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
 {
 _putchar('#');
 }
 _putchar('\n');
 }
 }
+}
+

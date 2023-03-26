d18147893da156d9ad67374fec85286d21a2340e
Timmy51 <adedejitimi5@gmail.com>
2023-03-25 23:19:18 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,20 +5,20 @@
  */
 int main(void)
 {
-int n = 1;
-while (n < 101)
-{
-if (n % 3 == 0 && n % 5 != 0)
-printf("Fizz");
-else if (n % 5 == 0 && n % 3 != 0)
-printf("Buzz");
-else if ((n % 3 == 0) && (n % 5 == 0))
-printf("FizzBuzz");
-else
-printf("%d", n);
+    int n = 1;
+    while (n < 101)
+    {
+        if (n % 3 == 0 && n % 5 != 0)
+            printf("Fizz");
+        else if (n % 5 == 0 && n % 3 != 0)
+            printf("Buzz");
+        else if (n % 3 == 0 && n % 5 == 0)
+            printf("FizzBuzz");
+        else
+            printf("%d", n);
 
-printf(" ");
-n++;
-}
-return (0);
+        printf(" ");
+        n++;
+    }
+    return (0);
 }

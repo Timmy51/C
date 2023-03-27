bc6d4483bdef12639f7da4d1ede754ac17706218
Timmy51 <adedejitimi5@gmail.com>
2023-03-27 12:02:04 -0700

    string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * _strlen -
+ * _strlen - returns the length of string
  * @s: a pointer variable
+ * Return: an integer
  */
 int _strlen(char *s)
 {
 int i = 0;
-while(s[i] != 0)
+while (s[i] != 0)
 i++;
-return (i + 1);
+return (i);
 }

df7bffd271e0807f801a4928818dbd6a0c5d2882
Timmy51 <adedejitimi5@gmail.com>
2023-03-27 12:21:54 -0700

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,9 +1,13 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _puts - returns the length of string
  * @str: a pointer variable
  */
 void _puts(char *str)
 {
-_putchar(str);
+int i = 0;
+while (str[i] != 0)
+_putchar(*str);
+i++;
 }

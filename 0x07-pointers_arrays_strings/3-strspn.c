de06c0540946944bba9065799002efed1d34c191
Timmy51 <adedejitimi5@gmail.com>
2023-04-03 06:16:43 -0700

    strchr

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,5 +7,19 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-
+unsigned int i = 0;
+int j;
+while (*s)
+{
+for (j = 0; accept(j); j++)
+{
+if (*s == accept[j])
+i++;
+break;
+else if (accept[j + 1] == 0)
+return (0);
+}
+s++
+}
+return (i)
 }

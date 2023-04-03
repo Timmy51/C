25982627d637a98aac5d399458c77ac6754ec110
Timmy51 <adedejitimi5@gmail.com>
2023-04-03 05:58:58 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * strchr - a function that locates a character in a string
+ * _strchr - a function that locates a character in a string
  * @s: a pointer variable
  * @c: a character variable
  * Return: a pointer to the first occurence of c

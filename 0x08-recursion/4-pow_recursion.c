0719974a6315a42f353707ca379fe717b0f74307
Timmy51 <adedejitimi5@gmail.com>
2023-04-05 06:19:54 -0700

    Taken hold

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _pow_recursion - a function that returns the value of x raised to the power of y
+ * _pow_recursion - a function that finds exponentials
  * @x: an integer variable x
  * @y: an integer variable y
  * Return: x raised to the power of y
@@ -15,7 +15,7 @@ else if (y == 0)
 {
 return (1);
 }
-else if(y == 1)
+else if (y == 1)
 {
 return (x);
 }

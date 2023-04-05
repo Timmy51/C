c139dd5428eb2407ca2eb6ebd6ca98b11651b003
Timmy51 <adedejitimi5@gmail.com>
2023-04-05 03:02:16 -0700

    She locked away a secret

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: a pointer character
+ */
+void _puts_recursion(char *s)
+{
+_putchar(s);
+_putchar('\n');
+}

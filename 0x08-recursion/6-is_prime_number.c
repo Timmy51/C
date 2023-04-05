cae5e722b2ae302b5df03080d1ceb6a8bd290a8c
Timmy51 <adedejitimi5@gmail.com>
2023-04-05 09:04:54 -0700

    The dreamer

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _is_prime_number  - finds a prime number
+ * _actual_recursion - the function where recursion is used
+ * @n: an integer variable n
+ * @i: an integer parameter
+ * Return: the square root
+ */
+int _int_prime_number(int n)
+{
+if (n % 2 != 0 && n % 3 != 0 && n % 5 != 0 && n % 7 != 0)
+{
+return (1);
+}
+else 
+return (0);
+}

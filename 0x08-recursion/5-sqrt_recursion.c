b53d51a147535ee37bce7a3b0492f44d0924c7c8
Timmy51 <adedejitimi5@gmail.com>
2023-04-05 06:37:31 -0700

    Im looking for a dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - finds the square root of a number
+ * @n: an integer variable n
+ * Return: the square root
+ */
+int _sqrt_recursion(int n)
+{
+int i = 0;
+i++;
+if (i * i = n)
+{
+return (i);
+}
+else return (-1);
+}

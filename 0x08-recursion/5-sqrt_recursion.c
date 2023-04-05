8bc5d236b0ad522c7c7457a671cd392a3c68ba26
Timmy51 <adedejitimi5@gmail.com>
2023-04-05 08:40:02 -0700

    Im looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,26 +4,27 @@
  * @n: an integer variable n
  * Return: the square root
  */
+
+int _actual_recursion(int n, int i);
+
 int _sqrt_recursion(int n)
 {
 if (n < 0)
 {
-return (-1)
+return (-1);
 }
-_actual_recursion(n, 0);
-return (i);
+return (_actual_recursion(n, 0));
 }
 
 int _actual_recursion(int n, int i)
 {
-int i;
 if (i *i == n)
 {
 return (i);
-return (_actual_recursion(n, i + 1));
 }
 if (i *i > n)
 {
 return (-1);
 }
+return (_actual_recursion(n, i + 1));
 }

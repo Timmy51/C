d74788bec21b7281ce6d98fb9faac2c3b461a193
Timmy51 <adedejitimi5@gmail.com>
2023-04-05 09:10:49 -0700

    The dreamer

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _is_prime_number  - finds a prime number
+ * is_prime_number  - finds a prime number
  * @n: an integer variable n
  * Return: 1 or 0
  */
-int _is_prime_number(int n)
+int is_prime_number(int n)
 {
 if (n % 2 != 0 && n % 3 != 0 && n % 5 != 0 && n % 7 != 0)
 {

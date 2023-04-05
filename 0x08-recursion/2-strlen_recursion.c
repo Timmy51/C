2b5281e695268dea11d7e07566d459a3d779f372
Timmy51 <adedejitimi5@gmail.com>
2023-04-05 04:40:03 -0700

    Dreams feel real when were in them

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recursion - a function that couts the length of string
+ * @s: a pointer variable
+ * Return: the length of the string
+ */
+int _strlen_recursion(char *s)
+{
+int i;
+i = 0;
+if (*s)
+{
+i++;
+}
+return (i);
+}

7469d69b8d8f88e8b9a4b1ef73fbe8dc7de3e380
Timmy51 <adedejitimi5@gmail.com>
2023-04-12 06:58:48 -0700

    Its not what they call you

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 /**
@@ -8,6 +9,6 @@
  */
 int main(int argc, char *argv[])
 {
-printf("%s\n", argv[0]);
+printf("%s", argv[0]);
 return (0);
 }

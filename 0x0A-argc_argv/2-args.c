216e0b79af55c7b2d5e45a411c0d8c1fa787139d
Timmy51 <adedejitimi5@gmail.com>
2023-04-12 07:49:57 -0700

    Silence is argument

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the name of the program
+ * @argc: argument count
+ * @argv:argument vector
+ * Return: 0 Success
+ */
+int main(int argc, char *argv[])
+int i;
+for (i = 1; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+return (0);
+}

70ae04b6511c509d7aad18a2d9da1a0c444bac24
Timmy51 <adedejitimi5@gmail.com>
2023-04-13 08:34:34 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,5 +8,15 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-
+int *ptr;
+if (nmemb == 0 || size == 0)
+{
+return (NULL);
+}
+ptr = malloc(sizeof(nmemb) * sizeof(size));
+if (ptr = NULL)
+{
+return (NULL);
+}
+return (ptr);
 }

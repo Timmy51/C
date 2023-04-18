377289444209662a8979f326b90485c7bff1ca11
Timmy51 <adedejitimi5@gmail.com>
2023-04-18 02:27:04 -0700

    A dog loves you

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,1 +1,22 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stddef.h>
 #include <stdio.h>
+/**
+ * init_dog - initializes a dog struct
+ * struct dog - struct dog declaration
+ * @d: th declared variable
+ * @name: the name of the dog
+ * @age: age of dog
+ * @owner: name of the owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d = malloc(sizeof(struct dog));
+if (d == NULL)
+printf("Error");
+
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
